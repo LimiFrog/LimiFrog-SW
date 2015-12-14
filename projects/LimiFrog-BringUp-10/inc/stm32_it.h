52d7716f49749c9d5f2a6461c373e050f5ef24a7
Xavier-C <xavier@LimiFrog.io>
2015-12-14 15:43:34 +0100

    Add unit test BringUp 10

---
+++ projects/LimiFrog-BringUp-10/inc/stm32_it.h
@@ -0,0 +1,29 @@
+/*******************************************************************************
+ * stm32_it.h
+ * 
+ * (c)2015 LimiFrog / CYMEYA
+ * This program is licensed under the terms of the MIT License.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+ * Please refer to the License File LICENSE.txt located at the root of this
+ * project for full licensing conditions, 
+ * or visit https://opensource.org/licenses/MIT.
+ ******************************************************************************/
+
+
+
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __STM32_IT_H
+#define __STM32_IT_H
+
+
+
+/* Exported functions ------------------------------------------------------- */
+
+
+
+
+#endif /* __STM32_IT_H */
+
+/***************************************************************END OF FILE****/
+
