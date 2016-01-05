3adedebfdb649981a9321d45d866c7916b2679d5
Xavier-C <xavier@LimiFrog.io>
2016-01-05 15:56:01 +0100

    update LSE-RTC setup for proper usage of external 32.768KHz in RTC periph

--- libraries/LimiFrog-Lib/src/Board_Support/LBF_LSE_RTC_Cfg.c
+++ libraries/LimiFrog-Lib/src/Board_Support/LBF_LSE_RTC_Cfg.c
@@ -9,11 +9,9 @@
  * or visit https://opensource.org/licenses/MIT.
  ******************************************************************************/
 
-
-
-
 #include "LBF_LSE_RTC_Cfg.h"
 #include "LBF_lowlev_API.h"
+
 
 /* Privates prototypes -------------------------------------------------------*/
 
@@ -29,6 +27,13 @@ void LBF_LSE_RTC_Cfg(void)
 RCC_OscInitTypeDef RCC_OscInitStruct = {0};
 RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
 
+  // To allow write access to RTC periph
+  __HAL_RCC_PWR_CLK_ENABLE(); //normally already done, also done in HAL_RCC_OscConfig...
+  HAL_PWR_EnableBkUpAccess();
+  __HAL_RCC_BACKUPRESET_FORCE(); // reset back-up domain
+  __HAL_RCC_BACKUPRESET_RELEASE();
+
+
   // Enable LSE Oscillator 
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSE;
   RCC_OscInitStruct.LSEState = RCC_LSE_ON; 
@@ -37,10 +42,17 @@ RCC_PeriphCLKInitTypeDef PeriphClkInit = {0};
     // Error_Handler();
   }
 
+
   // RTC Clock Inits
   PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_RTC;
-  PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;
-  HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit);
+  PeriphClkInit.RTCClockSelection = RCC_RTCCLKSOURCE_LSE;  // external 32.768KHz
+  if ( HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK )
+  { 
+    // Error_Handler();
+  }
+
+  // Enable RTC peripheral clock
+  __HAL_RCC_RTC_ENABLE();
 
 }
 
